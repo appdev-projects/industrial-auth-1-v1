162cfcd2c82c28822719b7cb9a297d5bfcffeb64
yanyuliu92 <yliu33@chicagobooth.edu>
2021-04-28 01:09:59 +0000

    Finished part 2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,12 @@
 class UsersController < ApplicationController
   before_action :set_user, only: %i[ show liked feed followers following discover ]
+  before_action :ensure_current_user_is_owner, only: [:feed, :discover ]
+
+  def feed    
+  end
+
+  def discover    
+  end
 
   private
 
@@ -8,6 +15,12 @@ class UsersController < ApplicationController
         @user = User.find_by!(username: params.fetch(:username))
       else
         @user = current_user
+      end
+    end
+
+    def ensure_current_user_is_owner
+      if params[:username].present? && current_user.username != params[:username]
+      redirect_back fallback_location: root_url, alert: "You're not authorized for that!"
       end
     end
 end
