6298d12460ca05e8735fc798c1fd902aff2fc28d
yepes0 <yepes0@chicagobooth.edu>
2021-04-30 01:11:44 +0000

    switch to next assignment

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: %i[ show liked feed followers following discover ]
+  before_action :ensure_current_user_is_feed_and_discover_owner, only: [:feed, :discover]
 
   private
 
@@ -10,4 +11,11 @@ class UsersController < ApplicationController
         @user = current_user
       end
     end
+
+    def ensure_current_user_is_feed_and_discover_owner
+      if current_user != @user
+        redirect_to "#{current_user}/feed" , alert: "You're not authorized to visit that page."
+      end
+    end
+
 end
