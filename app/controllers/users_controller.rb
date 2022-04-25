cc259741c29c2eb3298f7d889d4c72d8d7688fd0
anhdao21 <adao0@chicagobooth.edu>
2022-04-25 09:25:51 +0000

    hide_pending_and_feed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: %i[ show liked feed followers following discover ]
+  before_action :user_validation, only: %i[feed discover ]
 
   private
 
@@ -10,4 +11,10 @@ class UsersController < ApplicationController
         @user = current_user
       end
     end
+
+    def user_validation
+      if @user != current_user
+        redirect_back fallback_location: root_path, alert: "Nice try"
+      end
+    end 
 end
