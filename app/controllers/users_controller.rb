7d8e39c246628605e0eedd743ba49b660d86dc23
RilezLed <rledezma@chicagobooth.edu>
2022-04-26 07:26:25 +0000

    "updated private"

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,15 @@
 class UsersController < ApplicationController
   before_action :set_user, only: %i[ show liked feed followers following discover ]
+  before_action :ensure_user_public, only:[:show, :edit,:update,:destroy]
+
+  def ensure_user_public
+    if @user.private == true && @user.followers.find_by(username: current_user.username)== nil
+      #redirect_to root_path, alert:"Nice try,suckah"
+      redirect_back fallback_location: root_path, alert:  "Nice Try Sucka "
+      print(@user.followers.find_by(username: current_user.username)!= nil)
+      print("hello world")
+    end
+  end
 
   private
 
