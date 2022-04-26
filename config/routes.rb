829ede8f896fabaac2342c79ce172ca50b005f23
claycurran0 <ccurran@chicagobooth.edu>
2022-04-26 16:56:00 +0000

    hide other user feed and discover

--- config/routes.rb
+++ config/routes.rb
@@ -8,10 +8,10 @@ Rails.application.routes.draw do
   resources :likes
   resources :photos
 
+  get "feed" => "users#feed", as: :feed
+  get "discover" => "users#discover", as: :discover
   get ":username" => "users#show", as: :user
   get ":username/liked" => "users#liked", as: :liked
-  get ":username/feed" => "users#feed", as: :feed
-  get ":username/discover" => "users#discover", as: :discover
   get ":username/followers" => "users#followers", as: :followers
   get ":username/following" => "users#following", as: :following
 end
