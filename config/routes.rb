4e8e43714ab92da281efe41939977a2a8ba3c1d8
acuevasd <acuevasd@chicagobooth.edu>
2023-01-29 23:42:49 +0000

    adjust photos routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   resources :comments
   resources :follow_requests, except: [:index,:show,:new,:edit]
   resources :likes, only: [:create,:destroy]
-  resources :photos
+  resources :photos, except: [:index,:show]
 
   get ":username" => "users#show", as: :user
   get ":username/liked" => "users#liked", as: :liked
